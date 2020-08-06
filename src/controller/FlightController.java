116c169ef50bf7cf779ddb2e7c3b6b4b64e82c81
saikumar22 <saigoud2212@gmail.com>
2020-08-06 18:45:08 +0530

    FSJ0720A116

--- src/controller/FlightController.java
+++ src/controller/FlightController.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Flight;
+import services.FareCalculator;
+
 
 
 @WebServlet("/flight")
@@ -29,7 +32,7 @@ public class FlightController extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*		int numberOfPersons=Integer.parseInt(request.getParameter("persons"));
+		int numberOfPersons=Integer.parseInt(request.getParameter("persons"));
 		String classtype=request.getParameter("class");
 		String from=request.getParameter("from");
 		String to=request.getParameter("to");
@@ -45,7 +48,7 @@ public class FlightController extends HttpServlet {
 		LocalDate start=LocalDate.parse(from);		
 		LocalDate end=LocalDate.parse(to);
 
-		Flight flight=new Flight(numberOfPersons,rates,classtype,start,end,triptype);
+		Flight flight=new Flight(numberOfPersons,rates,start,end,triptype);
 		flight.setNoOfPersons(numberOfPersons);
 		flight.setRates(rates);
 		flight.setClassType(classtype);
@@ -59,6 +62,6 @@ public class FlightController extends HttpServlet {
 		request.setAttribute("flightfare", rate);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/flightView.jsp");
 		dispatcher.forward(request, response);
-	*/}
+	}
 
 }
